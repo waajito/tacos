9a2f6e565e33ba319da1359ff540178db5e72cac
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-09-18 03:49:04 +0530

    removed nextui and replaced with shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
 import { render } from "preact";
 import { App } from "./app.tsx";
 import "./index.css";
-import { NextUIProvider } from "@nextui-org/react";
 
-render(
-  <NextUIProvider>
-    <main className="dark text-foreground bg-background">
-      <App />
-    </main>
-  </NextUIProvider>,
-  document.getElementById("app")!
-);
+render(<App />, document.getElementById("app")!);

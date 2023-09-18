9c2f09ee8cb58d1536420e9eb8f62fd46322b1f4
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-09-18 14:35:41 +0530

    added components from shad

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import CreateOrJoinRoom from "./routes/createOrJoinRoom";
 import Router, { Route } from "preact-router";
 import WaitingRoom from "./routes/waitingRoom";
 import Room from "./routes/room";
+import { CardWithForm } from "./routes/test";
 
 export function App() {
   return (
@@ -10,6 +11,7 @@ export function App() {
       <Route path="/" component={CreateOrJoinRoom} />
       <Route path="/room" component={WaitingRoom} />
       <Route path="/room/:roomID" component={Room} />
+      <Route path="/test" component={CardWithForm} />
 
       <div default>sorry this page does not exist</div>
     </Router>
--- src/routes/createOrJoinRoom.tsx
+++ src/routes/createOrJoinRoom.tsx
@@ -1,9 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
 
 const CreateOrJoinRoom = () => {
   return (
     <div>
       CreateOrJoinRoom
+      <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
+        Taco<b>Bell</b>
+      </h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        Once upon a time, in a far-off land, there was a very lazy king who
+        spent all day lounging on his throne. One day, his advisors came to him
+        with a problem: the kingdom was running out of money.
+      </p>
+      <div className="flex flex-col space-y-1.5">
+        <Label htmlFor="name">Name</Label>
+        <Input
+          id="name"
+          placeholder="Name of your project"
+          className={"text-md height-10"}
+        />
+      </div>
+      <div>
+        <Label htmlFor="name">Name</Label>
+        <Input id="name" placeholder="Name of your project" />
+      </div>
       <Button>Button</Button>
     </div>
   );

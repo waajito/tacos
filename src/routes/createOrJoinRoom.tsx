eb63f672c491fe40d8069dfab89312d080603ebc
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-09-18 14:53:19 +0530

    add card look

--- src/routes/createOrJoinRoom.tsx
+++ src/routes/createOrJoinRoom.tsx
@@ -1,32 +1,44 @@
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
 
 const CreateOrJoinRoom = () => {
   return (
     <div>
-      CreateOrJoinRoom
-      <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
-        Taco<b>Bell</b>
-      </h1>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">
-        Once upon a time, in a far-off land, there was a very lazy king who
-        spent all day lounging on his throne. One day, his advisors came to him
-        with a problem: the kingdom was running out of money.
-      </p>
-      <div className="flex flex-col space-y-1.5">
-        <Label htmlFor="name">Name</Label>
-        <Input
-          id="name"
-          placeholder="Name of your project"
-          className={"text-md height-10"}
-        />
-      </div>
-      <div>
-        <Label htmlFor="name">Name</Label>
-        <Input id="name" placeholder="Name of your project" />
-      </div>
-      <Button>Button</Button>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>
+            <h3 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-3xl">
+              Taco<b>Bell</b>
+            </h3>
+          </CardTitle>
+          <CardDescription>
+            Appreciate your colleagues by giving out tacos.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">User Name</Label>
+                <Input id="name" placeholder="Your name" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Join</Button>
+          <Button>Create</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
